25e360be1fd05f70045bd25c8cf60fe5726fed83
Daniele Ricci <daniele.athome@gmail.com>
2014-05-28 10:45:54 +0200

    Fix span sizes on user form
    This crap will go away with the port to Bootstrap 3
    
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- htdocs/protected/views/user/_form.php
+++ htdocs/protected/views/user/_form.php
@@ -24,34 +24,36 @@
 
     <div class="input-prepend">
         <span class="add-on">@</span>
-        <?php echo $form->textField($model,'login',array('maxlength'=>140,'class'=>'span9','placeholder' => Yii::t('User', 'Login'))); ?>
+        <?php echo $form->textField($model,'login',array('maxlength'=>140,'class'=>'span12','placeholder' => Yii::t('User', 'Login'))); ?>
     </div>
 	<?php echo $form->error($model,'login', array('class' => 'alert alert-error')); ?>
 
     <div class="input-prepend">
         <span class="add-on"><i class="icon-lock"></i></span>
-	    <?php echo $form->passwordField($model,'password',array('value'=>'','maxlength'=>40,'class'=>'span9','placeholder' => Yii::t('User', 'Password'))); ?>
+	    <?php echo $form->passwordField($model,'password',array('value'=>'','maxlength'=>40,'class'=>'span12','placeholder' => Yii::t('User', 'Password'))); ?>
     </div>
 	<?php echo $form->error($model,'password', array('class' => 'alert alert-error')); ?>
 
     <div class="input-prepend">
         <span class="add-on"><i class="icon-user"></i></span>
-	    <?php echo $form->textField($model,'name',array('maxlength'=>255,'class'=>'span9','placeholder' => Yii::t('User', 'Name'))); ?>
+	    <?php echo $form->textField($model,'name',array('maxlength'=>255,'class'=>'span12','placeholder' => Yii::t('User', 'Name'))); ?>
 	</div>
 	<?php echo $form->error($model,'name', array('class' => 'alert alert-error')); ?>
 
     <div class="input-prepend">
         <span class="add-on"><i class="icon-flag"></i></span>
-	    <?php echo $form->textField($model,'role',array('maxlength'=>20,'class'=>'span9','placeholder' => Yii::t('User', 'Role'))); ?>
+	    <?php echo $form->textField($model,'role',array('maxlength'=>20,'class'=>'span12','placeholder' => Yii::t('User', 'Role'))); ?>
 	</div>
 	<?php echo $form->error($model,'role', array('class' => 'alert alert-error')); ?>
 
+	<div>
 	<?php echo CHtml::submitButton(Yii::t('app', $model->isNewRecord ? 'Create' : 'Save'), array('class' => 'btn btn-primary btn-large')); ?>
 	<?php if (!$model->isNewRecord): ?>
     <?php echo CHtml::link(Yii::t('app', 'Delete'), array('user/delete', 'id' => $model->id), array('class' => 'btn btn-large btn-danger',
         'submit' => array('user/delete', 'id' => $model->id),
         'confirm' => Yii::t('Page', 'Delete this user?'))); ?>
 	<?php endif; ?>
+	</div>
 
 	</div>
 </fieldset>

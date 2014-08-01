8a9658d9e6937f5c9213a81d8f3dc8a4c5f9656c
Daniele Ricci <daniele.athome@gmail.com>
2014-08-01 17:45:02 +0200

    Posts: port form to bs3
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- htdocs/protected/views/post/_form.php
+++ htdocs/protected/views/post/_form.php
@@ -1,8 +1,7 @@
-<div class="form">
-
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'post-form',
 	'enableAjaxValidation'=>false,
+    'htmlOptions' => array('role' => 'form'),
 )); ?>
 
 <fieldset>
@@ -14,32 +13,36 @@
     <?php endif; ?>
     </legend>
 
-	<?php echo $form->textField($model,'title',array('class'=>'span12','maxlength'=>200, 'placeholder' => Yii::t('Post', 'Title'))); ?>
-	<?php echo $form->error($model,'title',array('class' => 'alert alert-error')); ?>
+    <div class="form-group">
+	<?php echo $form->textField($model,'title',array('class'=>'form-control','maxlength'=>200, 'placeholder' => Yii::t('Post', 'Title'))); ?>
+	<?php echo $form->error($model,'title',array('class' => 'alert alert-danger')); ?>
+	</div>
 
-	<label class="checkbox" for="checkPublished">
+    <div class="checkbox">
+    <label class="checkbox" for="checkPublished">
 	<?php echo $form->checkBox($model,'published',array('id' => 'checkPublished', 'uncheckValue' => '', 'checked' => ($model->published !== null))); ?> <?php echo Yii::t('Post', 'Published'); ?>
 	</label>
+	</div>
 
-	<small class="muted">
+	<small class="text-muted">
 	<?php echo Yii::t('app', 'You may use <a target="_blank" href="{url}">Markdown syntax</a>.',
 	    array('{url}' => Yii::app()->params['markdownUrl'])); ?>
     </small>
 
-	<div>
-	<?php echo $form->textArea($model,'content',array('rows'=>10, 'cols'=>80, 'class' => 'post-content span12')); ?>
+	<div class="form-group">
+	<?php echo $form->textArea($model,'content',array('rows'=>10, 'cols'=>80, 'class' => 'post-content form-control')); ?>
 	</div>
 
-	<?php echo $form->textField($model,'tags',array('class'=>'span12','maxlength'=>200, 'placeholder' => Yii::t('Post', 'Tags'))); ?>
-	<?php echo $form->error($model,'tags',array('class' => 'alert alert-error')); ?>
+	<div class="form-group">
+	<?php echo $form->textField($model,'tags',array('class'=>'form-control','maxlength'=>200, 'placeholder' => Yii::t('Post', 'Tags'))); ?>
+	<?php echo $form->error($model,'tags',array('class' => 'alert alert-danger')); ?>
+	</div>
 
-	<?php echo CHtml::submitButton(Yii::t('app', $model->isNewRecord ? 'Create' : 'Save'), array('class' => 'btn btn-primary btn-large')); ?>
+	<?php echo CHtml::submitButton(Yii::t('app', $model->isNewRecord ? 'Create' : 'Save'), array('class' => 'btn btn-primary btn-lg')); ?>
 	<?php if (!$model->isNewRecord): ?>
-    <?php echo CHtml::link(Yii::t('app', 'Delete'), array('post/delete', 'id' => $model->id, 'language' => $model->language), array('class' => 'btn btn-large btn-danger', 'confirm' => Yii::t('Post', 'Delete {lang} version of this post?', array('{lang}' => $model->language)))); ?>
+    <?php echo CHtml::link(Yii::t('app', 'Delete'), array('post/delete', 'id' => $model->id, 'language' => $model->language), array('class' => 'btn btn-lg btn-danger', 'confirm' => Yii::t('Post', 'Delete {lang} version of this post?', array('{lang}' => $model->language)))); ?>
 	<?php endif; ?>
 
 </fieldset>
 
 <?php $this->endWidget(); ?>
-
-</div><!-- form -->

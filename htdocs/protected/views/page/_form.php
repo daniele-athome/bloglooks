828d6e0b5067d1cead17695facf8e0ba0f5bba58
Daniele Ricci <daniele.athome@gmail.com>
2014-08-01 15:54:01 +0200

    Page form ported to Bootstrap 3
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- htdocs/protected/views/page/_form.php
+++ htdocs/protected/views/page/_form.php
@@ -1,8 +1,7 @@
-<div class="form">
-
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'page-form',
 	'enableAjaxValidation'=>false,
+    'htmlOptions' => array('role' => 'form'),
 )); ?>
 
 <fieldset>
@@ -15,40 +14,43 @@
     </legend>
 
     <?php if ($model->isNewRecord): ?>
-	<?php echo $form->textField($model,'name',array('class'=>'span12','maxlength'=>100, 'placeholder' => Yii::t('Page', 'Name'))); ?>
-	<?php echo $form->error($model,'name',array('class' => 'alert alert-error')); ?>
-	<?php endif; ?>
+    <div class="form-group">
+    <?php echo $form->textField($model,'name',array('class'=>'form-control','maxlength'=>100, 'placeholder' => Yii::t('Page', 'Name'))); ?>
+    <?php echo $form->error($model,'name',array('class' => 'alert alert-error')); ?>
+    </div>
+    <?php endif; ?>
 
-	<?php echo $form->textField($model,'title',array('class'=>'span12','maxlength'=>200, 'placeholder' => Yii::t('Page', 'Title'))); ?>
-	<?php echo $form->error($model,'title',array('class' => 'alert alert-error')); ?>
+    <div class="form-group">
+    <?php echo $form->textField($model,'title',array('class'=>'form-control','maxlength'=>200, 'placeholder' => Yii::t('Page', 'Title'))); ?>
+    <?php echo $form->error($model,'title',array('class' => 'alert alert-error')); ?>
+    </div>
 
-	<label class="checkbox" for="checkPublished">
-	<?php echo $form->checkBox($model,'published',array('id' => 'checkPublished', 'uncheckValue' => '', 'checked' => ($model->published !== null))); ?> <?php echo Yii::t('Page', 'Published'); ?>
-	</label>
+    <div class="checkbox">
+    <label class="checkbox" for="checkPublished">
+    <?php echo $form->checkBox($model,'published',array('id' => 'checkPublished', 'uncheckValue' => '', 'checked' => ($model->published !== null))); ?> <?php echo Yii::t('Page', 'Published'); ?>
+    </label>
+    </div>
 
-	<!--
-	TODO sto coso qua...
-	<?php echo $form->labelEx($model,'order'); ?>
-	<?php echo $form->textField($model,'order',array('class'=>'span2','maxlength'=>5)); ?>
-	<?php echo $form->error($model,'order'); ?>
-	 -->
+    <!--
+    TODO sto coso qua...
+    <?php echo $form->labelEx($model,'order'); ?>
+    <?php echo $form->textField($model,'order',array('class'=>'form-control','maxlength'=>5)); ?>
+    <?php echo $form->error($model,'order'); ?>
+     -->
 
-	<small class="muted">
-	<?php echo Yii::t('app', 'You may use <a target="_blank" href="{url}">Markdown syntax</a>.',
-	    array('{url}' => Yii::app()->params['markdownUrl'])); ?>
+    <small class="text-muted">
+    <?php echo Yii::t('app', 'You may use <a target="_blank" href="{url}">Markdown syntax</a>.',
+        array('{url}' => Yii::app()->params['markdownUrl'])); ?>
     </small>
 
-	<div>
-	<?php echo $form->textArea($model,'content',array('rows'=>10, 'cols'=>80, 'class' => 'post-content span12')); ?>
-	</div>
+    <div class="form-group">
+    <?php echo $form->textArea($model,'content',array('rows'=>10, 'cols'=>80, 'class' => 'post-content form-control')); ?>
+    </div>
 
-	<?php echo CHtml::submitButton(Yii::t('app', $model->isNewRecord ? 'Create' : 'Save'), array('class' => 'btn btn-primary btn-large')); ?>
-	<?php if (!$model->isNewRecord): ?>
-    <?php echo CHtml::link(Yii::t('app', 'Delete'), array('page/delete', 'name' => $model->name, 'language' => $model->language), array('class' => 'btn btn-large btn-danger', 'confirm' => Yii::t('Page', 'Delete {lang} version of this page?', array('{lang}' => $model->language)))); ?>
-	<?php endif; ?>
+    <?php echo CHtml::submitButton(Yii::t('app', $model->isNewRecord ? 'Create' : 'Save'), array('class' => 'btn btn-primary btn-lg')); ?>
+    <?php if (!$model->isNewRecord): ?>
+    <?php echo CHtml::link(Yii::t('app', 'Delete'), array('page/delete', 'name' => $model->name, 'language' => $model->language), array('class' => 'btn btn-lg btn-danger', 'confirm' => Yii::t('Page', 'Delete {lang} version of this page?', array('{lang}' => $model->language)))); ?>
+    <?php endif; ?>
 
+    <?php $this->endWidget(); ?>
 </fieldset>
-
-<?php $this->endWidget(); ?>
-
-</div><!-- form -->
